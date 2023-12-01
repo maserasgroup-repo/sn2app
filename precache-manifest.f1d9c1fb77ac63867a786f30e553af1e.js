8bc973538dfbcf89506fb77b8c2237e7a0041982
lmoranglez <luciamoranglez@gmail.com>
2023-12-01 11:32:20 +0100

    Updates

---
+++ precache-manifest.f1d9c1fb77ac63867a786f30e553af1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "770177a6d5b7aaee3156",
+    "url": "/sn2app/static/css/main.2949130d.chunk.css"
+  },
+  {
+    "revision": "770177a6d5b7aaee3156",
+    "url": "/sn2app/static/js/main.770177a6.chunk.js"
+  },
+  {
+    "revision": "1dab174261534bc2133f",
+    "url": "/sn2app/static/js/1.1dab1742.chunk.js"
+  },
+  {
+    "revision": "5043c76003fbcb273cc1",
+    "url": "/sn2app/static/js/runtime~main.5043c760.js"
+  },
+  {
+    "revision": "786aead8c1e231361de29300fae74352",
+    "url": "/sn2app/index.html"
+  }
+];
